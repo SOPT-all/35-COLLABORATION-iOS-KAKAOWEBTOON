db5faa977ee566a34ac20b591a77b4bc007e6eb8
won <andrew112517@naver.com>
2024-11-19 16:40:21 +0900

    [Add] #11 - StorageView 파일 생성

---
+++ KAKAOWEBTOON-iOS/KAKAOWEBTOON-iOS/Presentation/Storage/View/StorageView.swift
@@ -0,0 +1,20 @@
+//
+//  StorageView.swift
+//  KAKAOWEBTOON-iOS
+//
+//  Created by 김승원 on 11/19/24.
+//
+
+import UIKit
+
+class StorageView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
