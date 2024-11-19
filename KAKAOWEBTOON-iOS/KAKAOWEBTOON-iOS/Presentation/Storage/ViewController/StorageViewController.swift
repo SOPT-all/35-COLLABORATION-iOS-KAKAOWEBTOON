44b781d6e9d3638572b6f844ef605a81d36f6575
won <andrew112517@naver.com>
2024-11-20 01:06:19 +0900

    [Add] #11 - 보관함 네비 타이틀 설정

--- KAKAOWEBTOON-iOS/KAKAOWEBTOON-iOS/Presentation/Storage/ViewController/StorageViewController.swift
+++ KAKAOWEBTOON-iOS/KAKAOWEBTOON-iOS/Presentation/Storage/ViewController/StorageViewController.swift
@@ -21,7 +21,33 @@ class StorageViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupNavigationBar()
+    }
+    
+    // MARK: - Private func
+    
+    private func setupNavigationBar() {
+        self.title = "보관함"
+        
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithDefaultBackground()
+        appearance.backgroundColor = .black3
+        appearance.shadowColor = nil
 
+        let titleFont = UIFont.appleSDGothicNeo(.title1_eb_17)
+        let titleAttributes: [NSAttributedString.Key: Any] = [
+          .font: titleFont,
+          .foregroundColor: UIColor.primaryWhite,
+          .kern: KakaoWebtoonFont.title1_eb_17.letterSpacing
+        ]
+
+        appearance.titleTextAttributes = titleAttributes
+
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        navigationController?.isNavigationBarHidden = false
     }
 
 }
